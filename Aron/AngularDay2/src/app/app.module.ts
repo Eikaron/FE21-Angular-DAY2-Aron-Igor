fe898390b49932455cdc42b6f9b9d4f8926fa417
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-10-12 15:32:41 +0200

    added contact, finished basic

--- Aron/AngularDay2/src/app/app.module.ts
+++ Aron/AngularDay2/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ContentComponent } from './content/content.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Aron/AngularDay2/src/app/contact/contact.component.ts
+++ Aron/AngularDay2/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'contact',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
+  
+  data = new FormGroup({
+    firstName: new FormControl("", [Validators.required, Validators.minLength(2)]),
+    lastName: new FormControl("", [Validators.required, Validators.minLength(2)]),
+    email: new FormControl("", Validators.required),
+    msg: new FormControl("", Validators.required)
+  });
 
   constructor() { }
+
+  formSubmit(){
+    if (this.data.valid){
+    console.log(this.data.value);
+    alert("Yay! It works")
+    }
+  }
 
   ngOnInit(): void {
   }
